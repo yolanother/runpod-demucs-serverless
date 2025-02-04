a7ca54ff19c3a284532a44970a4cb83dbd63360b
Yolan <yolanother@gmail.com>
2025-02-04 01:41:20 -0800

    Fix result encoding

--- src/handler.py
+++ src/handler.py
@@ -11,6 +11,10 @@ model = "mdx_extra"
 global separator
 separator = demucs.api.Separator(model="mdx_extra", segment=12)
 
+def log(message):
+    """ Helper function to log messages. """
+    print(f'[runpod-demucs-worker] {message}')
+
 def download_url_to_tempfile(url):
     """ Helper function to download a file from a URL to a temporary file. """
     import tempfile
@@ -32,26 +36,31 @@ def handler(job):
         separator = demucs.api.Separator(model=newmodel, segment=12)
     if 'url' in job_input:
         url = job_input['url']
+        log(f"Received job with URL: {url}, downloading...")
         input_file = download_url_to_tempfile(url)
+        log(f"Downloaded file from {url} to {input_file}")
+        # separate audio file
+        log("Separating audio file...")
         origin, separated = separator.separate_audio_file(input_file)
-        # compress separated into a zip and serve it back as a json base64
-        import zipfile
-        import base64
-        import io
-        import os
-        import json
+
         data = {
             "origin": origin,
             "files": separated,
-            "data": None
+            "files": []
         }
-        with io.BytesIO() as buf:
-            with zipfile.ZipFile(buf, 'w') as zipf:
-                zipf.write(origin, os.path.basename(origin))
-                for file in separated:
-                    zipf.write(file, os.path.basename(file))
-            buf.seek(0)
-            data['data'] = base64.b64encode(buf.read()).decode('utf-8')
+
+        # Remember to create the destination folder before calling `save_audio`
+        # Or you are likely to recieve `FileNotFoundError`
+        for stem in separated:
+            source = separated[stem]
+            log(f"Saving separated audio file {stem}...")
+            demucs.api.save_audio(source, f"{stem}.mp3", samplerate=separator.samplerate)
+            # Encode the file to base64 and add it to the files list {"filename": "file.mp3", "data": "base64encoded"}
+            with open(f"{stem}.mp3", "rb") as f:
+                data["files"].append({
+                    "filename": f"{stem}.mp3",
+                    "data": f.read().hex()
+                })
 
         return data
     else:
